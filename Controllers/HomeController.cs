3b08ce98e0cc8fba168b6eec7af2b78a119e90e5
SelinaHoHo <82096031+SelinaHoHo@users.noreply.github.com>
2022-12-23 22:30:08 +0700

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,32 +2,34 @@
 using ApplicationDev.Data;
 using Microsoft.AspNetCore.Mvc;
 using ApplicationDev.Models;
-
-namespace ApplicationDev.Controllers;
-
-public class HomeController : Controller
+namespace ApplicationDev.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
 
-     public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
             _context = context;
         }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            var obj = _context.Products.ToList();
+            return View(obj);
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
