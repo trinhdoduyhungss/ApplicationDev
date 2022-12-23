43ee84b6fe12bc1ea2f38d94d8edcd113a70d12a
phongttgcd <phongttgcd201885@fpt.edu.vn>
2022-12-23 22:42:17 +0700

    Init model CheckoutDetails

---
+++ Models/CheckoutDetails.cs
@@ -0,0 +1,12 @@
+namespace ApplicationDev.Models
+{
+    public class CheckoutDetails
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public int Quantity { get; set; }
+        
+    }
+}
