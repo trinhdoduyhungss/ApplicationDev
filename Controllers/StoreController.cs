dc6a2c1c3dfd7cb23271270bed047684350eaab1
phongttgcd <phongttgcd201885@fpt.edu.vn>
2022-12-24 18:27:40 +0700

    Update Store Controller
    Add CreateStore function

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -27,5 +27,32 @@ namespace ApplicationDev.Controllers
             var obj = await _storeService.GetAll();
             return View(obj);
         }
+        public IActionResult CreateStore()
+        {
+            var objId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (objId != null)
+            {
+                Store store = new Store
+                {
+                    UserList = _context.Users.ToList().Where(x=>x.Id == objId).Select(x=> new SelectListItem
+                    {
+                        Value = x.Id,
+                        Text = x.UserName
+                    })
+                };
+                return View(store);
+            }else
+            {
+                return RedirectToAction("Index", "Home");
+            }    
+        }
+        //Create
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+		public async Task<IActionResult> CreateStore(Store store)
+        {
+            await _storeService.Create(store);
+            return RedirectToAction(nameof(Index));
+        }
+        
     }
 }
